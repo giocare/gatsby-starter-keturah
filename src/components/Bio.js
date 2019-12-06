1ddeee4fdeaf61647b446a1db0e6dd49ae5cffbb
Toby <tobyau9000@gmail.com>
2019-12-05 23:47:46 -0500

    add container classname for css style

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -12,7 +12,7 @@ class Bio extends React.Component  {
         <div className="divider">
         </div>
 
-        <div>
+        <div className="about-content-container">
 
           <div className="comments">
               <p>// This is a comment.</p>
@@ -22,32 +22,32 @@ class Bio extends React.Component  {
 
             <div className="about-main-section">
              
-            <ul className="about-list">
-              <li>
-                <p><span style={{ color: "#FF97FF"}}>.name</span> &#123; </p>
-                  <p className="tag-info">Morgan MacGregor</p>
-                <p>&#125;</p>
-              </li>
-              <li>
-                <p><span style={{ color: "#86FFF8"}}>.bio</span> &#123; </p>
-                  <p className="tag-info">I am from New York, where she I work as a senior web engineer at Flywheel, WordPress hosting for creatives. I has 6 years of experience as a Full Stack engineer with a focus on APIs and creating responsive sites. In my spare time you can find me listening to true-crime podcasts, skateboarding in Prospect Park or teaching a spin class. </p>
-                <p>&#125;</p>
-              </li>
-              <li>
-                <p><span style={{ color: "#94D381"}}>.skills</span> &#123; </p>
-                  <p className="tag-info">Web: HTML;</p>
-                  <p className="tag-info">Web: CSS;</p>
-                  <p className="tag-info">Web: Javascript;</p>
-                  <p className="tag-info">Framework: React;</p>
-                  <p className="tag-info">Framework: Nodejs;</p>
-                  <p className="tag-info">Cloud Platform: Heroku;</p>
-                <p>&#125;</p>
-              </li>
-            </ul>
+              <ul className="about-list">
+                <li>
+                  <p><span style={{ color: "#FF97FF"}}>.name</span> &#123; </p>
+                    <p className="tag-info">Morgan MacGregor</p>
+                  <p>&#125;</p>
+                </li>
+                <li>
+                  <p><span style={{ color: "#86FFF8"}}>.bio</span> &#123; </p>
+                    <p className="tag-info">I am from New York, where she I work as a senior web engineer at Flywheel, WordPress hosting for creatives. I has 6 years of experience as a Full Stack engineer with a focus on APIs and creating responsive sites. In my spare time you can find me listening to true-crime podcasts, skateboarding in Prospect Park or teaching a spin class. </p>
+                  <p>&#125;</p>
+                </li>
+                <li>
+                  <p><span style={{ color: "#94D381"}}>.skills</span> &#123; </p>
+                    <p className="tag-info">Web: HTML;</p>
+                    <p className="tag-info">Web: CSS;</p>
+                    <p className="tag-info">Web: Javascript;</p>
+                    <p className="tag-info">Framework: React;</p>
+                    <p className="tag-info">Framework: Nodejs;</p>
+                    <p className="tag-info">Cloud Platform: Heroku;</p>
+                  <p>&#125;</p>
+                </li>
+              </ul>
 
-            <div className="about-image-container">
-              <img src={picture} /> 
-            </div>
+              <div className="about-image-container">
+                <img src={picture} /> 
+              </div>
 
             </div>  
                      
