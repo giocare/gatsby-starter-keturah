e6cf15fc460836286de0c1b2172131692e6c746a
Toby <tobyau9000@gmail.com>
2019-12-06 21:30:57 -0500

    added class and updated logic to support page redirect with componendDidMount

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,120 +1,132 @@
-import React from 'react'
+import React , { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import Typist from 'react-typist'
 import Delay from 'react-delay'
 
-const Terminal = ({ data }) => {
+class Terminal extends Component {
   
-  const { type, command1, command2, name, description } = data.edges[0].node.frontmatter;
-  
-  var description_list = [];
-  var sentence = "";
-  for(var i = 0; i < description.length; i++) {
-    if(description[i] === '>' || i === description.length-1) {
-      description_list.push(sentence.trim());
-      sentence = "";
-    }
-    else {
-      sentence += description[i];
+  componentDidMount() {
+    if(window.location.href === "http://localhost:8000/") {
+      setTimeout(() => {
+        window.location.href = '/about';
+      }, 28000);
     }
   }
   
-  if(window.location.href === "http://localhost:8000/") {
-    setTimeout(() => {
-      window.location.href = '/about';
-    }, 28000);
-  }
-  
-  return (
-    <div className="terminal-container">
-      <nav className="terminal-nav">
-        <div className="circles-container">
-          <div className="circle red" style={{ backgroundColor: "#ED3131"}}></div>
-          <div className="circle yellow" style={{ backgroundColor: "#EDCF31"}}></div>
-          <div className="circle green" style={{ backgroundColor: "#4CED31"}}></div>
+  render() {
+    const { command1, command2, name, description } = this.props.data;
+    
+    var description_list = [];
+    var sentence = "";
+    for(var i = 0; i < description.length; i++) {
+      if(description[i] === '>' || i === description.length-1) {
+        description_list.push(sentence.trim());
+        sentence = "";
+      }
+      else {
+        sentence += description[i];
+      }
+    }
+    
+    return (
+      <div className="terminal-container">
+        <nav className="terminal-nav">
+          <div className="circles-container">
+            <div className="circle red" style={{ backgroundColor: "#ED3131"}}></div>
+            <div className="circle yellow" style={{ backgroundColor: "#EDCF31"}}></div>
+            <div className="circle green" style={{ backgroundColor: "#4CED31"}}></div>
+          </div>
+          <div className="terminal-title">bash</div>
+        </nav>
+        <div className="row">
+          <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
+          <Typist 
+            cursor={{ hideWhenDone: true }}
+          >
+            {command1}
+          </Typist>
         </div>
-        <div className="terminal-title">bash</div>
-      </nav>
-      <div className="row">
-        <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
-        <Typist 
-          cursor={{ hideWhenDone: true }}
-        >
-          {command1}
-        </Typist>
+        
+        <Delay wait={3000}>
+          <div className="row">
+            <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
+            <Typist 
+              cursor={{ hideWhenDone: true }}
+            >
+              {command2}
+            </Typist>
+          </div>
+        </Delay>
+        
+        <Delay wait={6000}>
+          <div className="row">
+            <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
+            <Typist 
+              cursor={{ hideWhenDone: true }}
+            >
+              Hi I'm {name}
+            </Typist>
+          </div>
+        </Delay>
+        
+        <Delay wait={10000}>
+          <div className="row">
+            <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
+            <Typist 
+              cursor={{ hideWhenDone: true }}
+            >
+              {description_list[0]}
+            </Typist>
+          </div>
+        </Delay>
+        
+        <Delay wait={14000}>
+          <div className="row">
+            <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
+            <Typist 
+              cursor={{ hideWhenDone: true }}
+            >
+              {description_list[1]}
+            </Typist>
+          </div>
+        </Delay>
+        
+        <Delay wait={19000}>
+          <div className="row">
+            <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
+            <Typist 
+              cursor={{ hideWhenDone: true }}
+            >
+              {description_list[2]}
+            </Typist>
+          </div>
+        </Delay>
+        
+        <Delay wait={23000}>
+          <div className="row">
+            <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
+            Sign In (Yes / No): &nbsp;
+            <Typist 
+              cursor={{ hideWhenDone: true }}
+            >
+              <Typist.Delay ms={900} />
+              Yes
+            </Typist>
+          </div>
+        </Delay>
       </div>
-      
-      <Delay wait={3000}>
-        <div className="row">
-          <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
-          <Typist 
-            cursor={{ hideWhenDone: true }}
-          >
-            {command2}
-          </Typist>
-        </div>
-      </Delay>
-      
-      <Delay wait={6000}>
-        <div className="row">
-          <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
-          <Typist 
-            cursor={{ hideWhenDone: true }}
-          >
-            Hi I'm {name}
-          </Typist>
-        </div>
-      </Delay>
-      
-      <Delay wait={10000}>
-        <div className="row">
-          <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
-          <Typist 
-            cursor={{ hideWhenDone: true }}
-          >
-            {description_list[0]}
-          </Typist>
-        </div>
-      </Delay>
-      
-      <Delay wait={14000}>
-        <div className="row">
-          <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
-          <Typist 
-            cursor={{ hideWhenDone: true }}
-          >
-            {description_list[1]}
-          </Typist>
-        </div>
-      </Delay>
-      
-      <Delay wait={19000}>
-        <div className="row">
-          <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
-          <Typist 
-            cursor={{ hideWhenDone: true }}
-          >
-            {description_list[2]}
-          </Typist>
-        </div>
-      </Delay>
-      
-      <Delay wait={23000}>
-        <div className="row">
-          <FontAwesomeIcon icon={ faAngleRight } className="angle-right" style={{ color: 'white', fontSize:'15px'}}/>
-          Sign In (Yes / No): &nbsp;
-          <Typist 
-            cursor={{ hideWhenDone: true }}
-          >
-            <Typist.Delay ms={900} />
-            Yes
-          </Typist>
-        </div>
-      </Delay>
-      
+    )
+  }
+}
+
+
+const TerminalFunc = ({ data }) => {
+  return (
+    <div>
+      <Terminal data={data.edges[0].node.frontmatter} />
     </div>
   );
 }
 
-export default Terminal; 
+export default TerminalFunc; 
